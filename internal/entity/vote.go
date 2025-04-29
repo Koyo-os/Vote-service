524b453b57ab72022e2648a7815c1cc5acb1a4d1
Osamikoyo <v.pylayev@mail.ru>
2025-04-29 05:56:31 +0500

    update vote entity

--- internal/entity/vote.go
+++ internal/entity/vote.go
@@ -6,13 +6,17 @@ type Vote struct {
 	ID      uuid.UUID `json:"id" gorm:"type:uuid;primaryKey;default:uuid_generate_v4()"`
 	PollID  uuid.UUID `json:"poll_id" gorm:"type:uuid"`
 	FieldID uint      `json:"field_id"`
+	UserID  uuid.UUID `json:"user_id"`
 }
 
-func NewVote(PollID string, fieldID uint) *Vote {
+func NewVote(PollID string, fieldID uint, userId string) *Vote {
 	pollid, _ := uuid.Parse(PollID)
+	userid, _ := uuid.Parse(userId)
+
 	return &Vote{
 		ID:      uuid.New(),
 		PollID:  pollid,
 		FieldID: fieldID,
+		UserID:  userid,
 	}
 }
