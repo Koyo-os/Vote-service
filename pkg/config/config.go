0784f84b4de30c17ffffe54e5fff6bcd7aca6890
Osamikoyo <v.pylayev@mail.ru>
2025-05-01 06:27:27 +0500

    update config

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -6,17 +6,16 @@ import (
 )
 
 type Config struct {
-	KAFKA_URL         string `env:"KAFKA_URL" envDefault:"localhost:9092"`
-	DSN               string `env:"DSN" envDefault:"data/main.db"`
-	CreateVoteReqType string `env:"CREATE_REQ_TYPE" envDefault:"request.vote.create"`
+	RabbitMQUrl       string `env:"RABBITMQ_URL"            envDefault:"localhost:9092"`
+	DSN               string `env:"DSN"                     envDefault:"data/main.db"`
+	CreateVoteReqType string `env:"CREATE_REQ_TYPE"         envDefault:"request.vote.create"`
 	CreatedVoteType   string `env:"CREATED_VOTE_EVENT_TYPE" envDefault:"vote.created"`
-	DeleteVoteReqType string `env:"DELETE_REQ_TYPE" envDefault:"request.vote.delete"`
+	DeleteVoteReqType string `env:"DELETE_REQ_TYPE"         envDefault:"request.vote.delete"`
 	DeletedVoteType   string `env:"DELETED_VOTE_EVENT_TYPE" envDefault:"vote.deleted"`
-	RequestTopicName  string `env:"REQUEST_TOPIC_NAME" envDefault:"requests"`
-	TopicName         string `env:"TOPIC_NAME" envDefault:"events"`
-	GroupID           string `env:"GROUP_ID" envDefault:"vote"`
-	RedisSentinelUrl  string `env:"REDIS_SENTINEL_HOST" envDefault:"redis-sentinel"`
-	RedisPassword     string `env:"REDIS_PASSWORD" envDefault:"redis"`
+	RequestTopicName  string `env:"REQUEST_TOPIC_NAME"      envDefault:"requests"`
+	OutputTopicName   string `env:"OUTPUT_TOPIC_NAME"       envDefault:"events"`
+	GroupID           string `env:"GROUP_ID"                envDefault:"vote"`
+	RedisUrl          string `env:"REDIS_URL"               envDefault:"localhost:3056"`
 }
 
 func Load() (*Config, error) {
@@ -27,8 +26,6 @@ func Load() (*Config, error) {
 	if err := env.Parse(&cfg); err != nil {
 		return nil, err
 	}
-
-	cfg.RedisSentinelUrl = cfg.RedisSentinelUrl + ":26379"
 
 	return &cfg, nil
 }
