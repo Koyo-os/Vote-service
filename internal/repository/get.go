0e70e5c0f3b80d7c071b0406bbd50b9b8468292e
Osamikoyo <v.pylayev@mail.ru>
2025-05-18 05:37:42 +0500

    update repo func

--- internal/repository/get.go
+++ internal/repository/get.go
@@ -5,9 +5,13 @@ import (
 
 	"github.com/Koyo-os/Vote-service/internal/entity"
 	"github.com/google/uuid"
+	"go.uber.org/zap"
 )
 
-func (repoImpl *VoteRepositoryImpl) GetByUserAndPollID(ctx context.Context, userID, pollID uuid.UUID) ([]entity.Vote, error) {
+func (repoImpl *VoteRepositoryImpl) GetByUserAndPollID(
+	ctx context.Context,
+	userID, pollID uuid.UUID,
+) ([]entity.Vote, error) {
 	var votes []entity.Vote
 
 	res := repoImpl.db.Where(&entity.Vote{
@@ -16,4 +20,26 @@ func (repoImpl *VoteRepositoryImpl) GetByUserAndPollID(ctx context.Context, user
 	}).Find(&votes)
 
 	return votes, res.Error
+}
+
+func (repoImpl *VoteRepositoryImpl) GetByID(
+	ctx context.Context,
+	id uuid.UUID,
+) (*entity.Vote, error) {
+	var vote entity.Vote
+
+	res := repoImpl.db.Where(&entity.Event{
+		ID: id.String(),
+	}).Find(&vote)
+
+	if err := res.Error; err != nil {
+		repoImpl.logger.Error(
+			"error get vote from db",
+			zap.String("vote_id", id.String()),
+			zap.Error(err),
+		)
+		return nil, err
+	}
+
+	return &vote, nil
 }
